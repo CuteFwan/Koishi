4ed5d39826cbbab52c275cd86f9cd1f1a197311b
CuteFwan <flandre.scarlet66@gmail.com>
2019-04-13 20:27:13 -0400

    lowered logging

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ AVY_CHANNEL = config["AVY_CHANNEL"]
 DEFAULT_PREFIX = config["DEFAULT_PREFIX"]
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
