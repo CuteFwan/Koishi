75a10e601309b6db28da3cfb905d4fa088cd5a82
CuteFwan <flandre.scarlet66@gmail.com>
2019-09-24 16:07:06 -0400

    Added extension loading, unloading, and reloading to admin cog.

--- cogs/admin.py
+++ cogs/admin.py
@@ -19,6 +19,33 @@ class Admin(commands.Cog):
         return ctx.author.id in self.bot.admins
 
     @commands.command(hidden=True)
+    async def load(ctx, extension_name: str):
+        try:
+            bot.load_extension(extension_name)
+        except (AttributeError, ImportError) as e:
+            await ctx.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+            return
+        logger.info(f'{extension_name} loaded.')
+        await ctx.send(f'{extension_name} loaded.')
+
+    @commands.command(hidden=True)
+    async def unload(ctx, extension_name: str):
+        bot.unload_extension(extension_name)
+        logger.info(f'{extension_name} unloaded.')
+        await ctx.send(f'{extension_name} unloaded.')
+
+    @commands.command(hidden=True)
+    async def reload(ctx, extension_name: str):
+        bot.unload_extension(extension_name)
+        try:
+            bot.load_extension(extension_name)
+        except (AttributeError, ImportError) as e:
+            await ctx.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+            return
+        logger.info(f'{extension_name} reloaded.')
+        await ctx.send(f'{extension_name} reloaded.')
+
+    @commands.command(hidden=True)
     async def sql(self, ctx, *, query):
         """Copied from old bot"""
         start = time.perf_counter()
